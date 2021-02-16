981d7c5e7ef1466840cc15f3b949c1e4abdd7353
Nucleardelta <ssignmeup@live.com>
2021-02-15 23:46:00 -0500

    ascas

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $(".round-page-num").click(function(){
-      gsap.to(window, {duration: .95, scrollTo:$(this).data("dest"), ease:Power4.easeInOut});
+      gsap.to(window, {duration: .95, scrollTo:$(this).data("dest"),ease:Power4.easeInOut});
   });
 });
