4e2a19b20a1d3fdcd38a52064b2b6f38f81af8d5
Nucleardelta <ssignmeup@live.com>
2021-02-25 21:48:24 -0500

    perfected note popup

--- js/scripts.js
+++ js/scripts.js
@@ -17,18 +17,38 @@ $(document).ready(function(){
 
     /*-------------------SUPPORT FUNCTIONS-----------------*/
         function showBlurb(blurbIndex){
-            theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".topspear")  ,  {scale:0,x:-100});
-            theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".bottomspear"), {scale:0,x:100});
-            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".topspear"),  6, {scale:1,x:-200,delay:2});
-            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".bottomspear"),6,{scale:1,x:200,delay:2});
+            theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".tl")  ,{width:5});
+            theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".tr"), {height:5});
+            theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".br"), {width:5});
+            theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".bl")  ,  {height:5});
 
-            // theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".image-text"),{rotationY:"90deg"});
-            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".image-text"),6.5,{rotationY:"0deg",opacity:1,ease:Back.easeOut,delay:2});
+            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".tl"),5.5  ,{width:"100%",delay:2});
+            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".tr"),5.5, {height:"100%",delay:2});
+            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".br"),5.5, {width:"100%",delay:2});
+            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".bl"),5.5  ,  {height:"100%",delay:2});
+
+            theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".image-text"),{scale:1.2});
+            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".image-text"),2,{scale:1,delay:2});
+            theGSAPInstance = gsap.to($(picArray[blurbIndex]).find(".image-text"),6.5,{opacity:1,ease:Back.easeOut,delay:2});
             theGSAPInstance = gsap.to(picArray[blurbIndex],.95,{opacity:0,delay:12.5,onComplete:function(){
                 theGSAPInstance = gsap.set($(picArray[blurbIndex]).find(".image-text"),{opacity:0});
                 currPic = blurbIndex === 0 ? picArray.length-1 : blurbIndex - 1 ;
                 fadeIn(currPic);
             }});
+        }
+        function showBlurb2(blurbIndex){
+            gsap.set($(picArray[blurbIndex]).find(".tl")  ,{width:5});
+            gsap.set($(picArray[blurbIndex]).find(".tr"), {height:5});
+            gsap.set($(picArray[blurbIndex]).find(".br"), {width:5});
+            gsap.set($(picArray[blurbIndex]).find(".bl")  ,  {height:5});
+
+            gsap.to($(picArray[blurbIndex]).find(".tl"),2.5  ,{width:"100%"});
+            gsap.to($(picArray[blurbIndex]).find(".tr"),2.5, {height:"100%"});
+            gsap.to($(picArray[blurbIndex]).find(".br"),2.5, {width:"100%"});
+            gsap.to($(picArray[blurbIndex]).find(".bl"),2.5  ,  {height:"100%"});
+
+            gsap.set($(picArray[blurbIndex]).find(".image-text"),{scale:1.2});
+            gsap.to($(picArray[blurbIndex]).find(".image-text"),1.5,{scale:1,rotationY:"0deg",opacity:1});
         }
         function appendThumb(){
             var thumbID;
@@ -54,31 +74,11 @@ $(document).ready(function(){
             picArray.forEach((img,i)=>{
                 if(indx === i){
                     gsap.to(img,1,{opacity:1});
+                    showBlurb2(indx);
                 }else{
                     gsap.to(img,1,{opacity:0});
                 }
             });
-            textArray.forEach((txtBlok,j)=>{
-                gsap.to(txtBlok,.3,{opacity:0});
-            });
-            $(".read-tests").delay(300).fadeIn();
-        }
-
-        function readTests(){
-            $(".read-tests").delay(300).fadeOut();
-            $(".view-cover").delay(300).fadeIn();
-            textArray.forEach((txtBlok,i)=>{
-                if(currPic === i){
-                    gsap.to(txtBlok,1,{opacity:1});
-                }else{
-                    gsap.to(txtBlok,1,{opacity:0});
-                }
-            });
-        }
-        function viewCover(){
-            $(".read-tests").delay(300).fadeIn();
-            $(".view-cover").delay(300).fadeOut();
-            $(".image-text").fadeOut();
         }
     /*--------------------END SUPPORT FUNCTIONS-----------------*/
 
@@ -90,6 +90,11 @@ $(document).ready(function(){
     $(".round").click(function(){
         theGSAPInstance.kill();
         fadeIn2( parseInt($(this).data('index')) );
+    });
+    /*------CLICKING THE IMAGE------*/
+    $(".image,#slideControl").click(function(){
+        theGSAPInstance.kill();
+        fadeIn2( picArray.length - 1- currPic );
     });
     /*------CLICKING READ TESTS------*/
     $(".read-tests").click(function(){
