7d3835d801f0b116e7cacc3df238ad8ad8b183f6
Nucleardelta <ssignmeup@live.com>
2021-02-26 19:07:54 -0500

    Almost ready

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
     let picArray = gsap.utils.toArray(".image");
+    let picArrayRev = gsap.utils.toArray(".image").reverse();
     let textArray = gsap.utils.toArray(".image-text");
     let currPic = picArray.length-1;
     var theGSAPInstance;
 
     function fadeIn(picIndex){
         highlightThumb();
+        $(".bigBackText,.middleBackText,.smallBackText").fadeOut(1000);
+        $(".bigBackText,.middleBackText,.smallBackText").html( $(picArray[picIndex]).find(".the-actual-text").html() );
+        $(".bigBackText,.middleBackText,.smallBackText").fadeIn();
         theGSAPInstance = gsap.set($(picArray[picIndex]).children(".img-hldr"),{backgroundSize:"120% 120%",backgroundPosition:"center center"});
 
         theGSAPInstance = gsap.to(picArray[picIndex],.95,{opacity:1});
@@ -52,14 +56,13 @@ $(document).ready(function(){
         }
         function appendThumb(){
             var thumbID;
-            picArray.forEach((thumb,i)=>{
-                thumbID = picArray.length-1-i;
+            picArrayRev.forEach((thumb,i)=>{
+                thumbID = i;
                 $(".magazine").append("<div class='round' id='thumb_"+thumbID+"' data-index='"+thumbID+"' style='background-image:"+$(thumb).children(".img-hldr").css("background-image")+"'></div>");
             });
         }
         function highlightThumb(){
             picArray.forEach((thumb,i)=>{
-
                 if( i === picArray.length-1-currPic ){
                     $("#thumb_"+i).addClass("actv");
                 }else{
@@ -79,29 +82,45 @@ $(document).ready(function(){
                     gsap.to(img,1,{opacity:0});
                 }
             });
+            //now highlight the thumb
+            thumbArray.forEach((thumb,j)=>{
+                if( j === ind ){
+                    $("#thumb_"+j).addClass("actv");
+                }else{
+                    $("#thumb_"+j).removeClass("actv");
+                }
+            });
         }
     /*--------------------END SUPPORT FUNCTIONS-----------------*/
 
-
+  /*``````````````````````````/
+ /     call append thumb    /
+/........................*/
     appendThumb();
+    let thumbArray = gsap.utils.toArray(".round");
+    /*````````````````````````````````````/
+  /     call main function fadeIn()     /
+/....................................*/
     fadeIn(currPic);
+    $(".catshield").fadeOut(5000);
+    // gsap.to($(".main-title-frame-about,.inner-cartridge-about"),5,{width:"105%"});
+
+    /*---------START TEXTFLOW--------*/
+    gsap.to($(".bigBackText"),2100,{x:-10000});
+    gsap.to($(".middleBackText"),2100,{x:-20000});
+    gsap.to($(".smallBackText"),2100,{x:-40000});
+
 
     /*------CLICKING A THUMBNAIL------*/
     $(".round").click(function(){
         theGSAPInstance.kill();
         fadeIn2( parseInt($(this).data('index')) );
     });
-    /*------CLICKING THE IMAGE------*/
+    /*------CLICKING THE IMAGE OR THE PAUSE BUTTON------*/
     $(".image,#slideControl").click(function(){
         theGSAPInstance.kill();
         fadeIn2( picArray.length - 1- currPic );
-    });
-    /*------CLICKING READ TESTS------*/
-    $(".read-tests").click(function(){
-        readTests();
-    });
-    /*------CLICKING VIEW COVER------*/
-    $(".view-cover").click(function(){
-        viewCover();
+        /*slideControlHldr
+<div id="slideControl">PAUSE SLIDESHOW</div>*/
     });
 });
