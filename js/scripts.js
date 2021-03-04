14ca16354c6bb28151ed49dea6ef163c59e1d610
Nucleardelta <ssignmeup@live.com>
2021-03-04 15:23:00 -0500

    changed things over to the 1-line format

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
 
-    let picArray = gsap.utils.toArray(".image");
-    let picArrayRev = gsap.utils.toArray(".image").reverse();
-    let textArray = gsap.utils.toArray(".image-text");
+    let picArray = gsap.utils.toArray(".main-frame");
     let currPic = picArray.length-1;
-    var theGSAPInstance;
 
     function fadeIn(picIndex){
         /*---begin textflow---*/
@@ -14,31 +11,30 @@ $(document).ready(function(){
         }});
         /*---end textflow---*/
 
-        if($(picArray[picIndex]).find(".image-text").length !== 0){ //if it's a text div
+    //if it's a text div
+        if($(picArray[picIndex]).find(".image-text").length !== 0){
             gsap.set($(picArray[picIndex]).find(".image-text"),{scale:1.2});
+            gsap.set($(picArray[picIndex]),{opacity:1});
 
             gsap.to($(picArray[picIndex]).find(".image-text"),2,{scale:1});
             gsap.to($(picArray[picIndex]).find(".image-text"),6.5,{opacity:1});
 
             // here
             let nextIndex = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
-            gsap.set($(picArray[nextIndex]).find(".img-hldr"),{opacity:0,backgroundSize:"120% 120%",backgroundPosition:"center center"});
+            gsap.set($(picArray[nextIndex]),{opacity:0,backgroundSize:"120% 120%",backgroundPosition:"center center"});
 
-            gsap.to($(picArray[nextIndex]).find(".img-hldr"),1.95,{opacity:1,delay:7});
-            gsap.to($(picArray[nextIndex]).find(".img-hldr"),3,{ backgroundSize:"100% 100%",delay:6.5 });
+            gsap.to($(picArray[nextIndex]),5,{opacity:1,ease:Power4.easeIn,delay:5.5});
+            gsap.to($(picArray[nextIndex]),5,{ backgroundSize:"100% 100%",ease:Power4.easeInOut,delay:6.5 });
             //end here
 
-            gsap.to($(picArray[picIndex]).find(".image-text"),1.95,{opacity:0,scale:0.8,delay:6.5,onComplete:function(){
+            gsap.to($(picArray[picIndex]).find(".image-text"),2,{opacity:0,scale:.8,delay:7.5,onComplete:function(){
                 currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
                 fadeIn(currPic);
             }});
         }
-        else{ //if it's an image div
-            // gsap.set($(picArray[picIndex]).find(".img-hldr"),{opacity:0,backgroundSize:"120% 120%",backgroundPosition:"center center"});
-            //
-            // gsap.to($(picArray[picIndex]).find(".img-hldr"),.95,{opacity:1});
-            // gsap.to($(picArray[picIndex]).find(".img-hldr"),3,{ backgroundSize:"100% 100%" });
-            gsap.to($(picArray[picIndex]).find(".img-hldr"),.95,{backgroundSize:"95% 95%",opacity:0,delay:5,onComplete:function(){
+    //if it's an image div
+        else{
+            gsap.to($(picArray[picIndex]),.95,{backgroundSize:"95% 95%",opacity:0,delay:5,onComplete:function(){
                 currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
                 fadeIn(currPic);
             }});
@@ -47,26 +43,6 @@ $(document).ready(function(){
     }
 
     /*-------------------SUPPORT FUNCTIONS-----------------*/
-        function showBlurb(blurbIndex){
-
-            gsap.set($(picArray[blurbIndex]).find(".image-text"),{scale:1.2});
-            gsap.to($(picArray[blurbIndex]).find(".image-text"),2,{scale:1,delay:2});
-            gsap.to($(picArray[blurbIndex]).find(".image-text"),6.5,{opacity:1,ease:Back.easeOut,delay:2});
-            gsap.to(picArray[blurbIndex],.95,{opacity:0,delay:12.5,onComplete:function(){
-                gsap.set($(picArray[blurbIndex]).find(".image-text"),{opacity:0});
-                currPic = blurbIndex === 0 ? picArray.length-1 : blurbIndex - 1 ;
-                fadeIn(currPic);
-            }});
-        }
-        function highlightThumb(){
-            picArray.forEach((thumb,i)=>{
-                if( i === picArray.length-1-currPic ){
-                    $("#thumb_"+i).addClass("actv");
-                }else{
-                    $("#thumb_"+i).removeClass("actv");
-                }
-            });
-        }
 
     /*--------------------END SUPPORT FUNCTIONS-----------------*/
 
---
+++ js/scripts_alt.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+
+    let picArray = gsap.utils.toArray(".image");
+    let picArrayRev = gsap.utils.toArray(".image").reverse();
+    let textArray = gsap.utils.toArray(".image-text");
+    let currPic = picArray.length-1;
+    var theGSAPInstance;
+
+    function fadeIn(picIndex){
+        /*---begin textflow---*/
+        gsap.to($(".bigBackText,.middleBackText,.smallBackText"),1,{opacity:0,onComplete:function(){
+            $(".bigBackText,.middleBackText,.smallBackText").html( $(picArray[picIndex]).find(".the-actual-text").html() );
+            gsap.to($(".bigBackText,.middleBackText,.smallBackText"),2,{opacity:1});
+        }});
+        /*---end textflow---*/
+
+        if($(picArray[picIndex]).find(".image-text").length !== 0){ //if it's a text div
+            gsap.set($(picArray[picIndex]).find(".image-text"),{scale:1.2});
+
+            gsap.to($(picArray[picIndex]).find(".image-text"),2,{scale:1});
+            gsap.to($(picArray[picIndex]).find(".image-text"),6.5,{opacity:1});
+
+            // here
+            let nextIndex = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
+            gsap.set($(picArray[nextIndex]).find(".img-hldr"),{opacity:0,backgroundSize:"120% 120%",backgroundPosition:"center center"});
+
+            gsap.to($(picArray[nextIndex]).find(".img-hldr"),5,{opacity:1,delay:6.5});
+            gsap.to($(picArray[nextIndex]).find(".img-hldr"),3,{ backgroundSize:"100% 100%",delay:6.5 });
+            //end here
+
+            gsap.to($(picArray[picIndex]).find(".image-text"),.3,{opacity:0,scale:.1,delay:6.5,onComplete:function(){
+                currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
+                fadeIn(currPic);
+            }});
+        }
+        else{ //if it's an image div
+            // gsap.set($(picArray[picIndex]).find(".img-hldr"),{opacity:0,backgroundSize:"120% 120%",backgroundPosition:"center center"});
+            //
+            // gsap.to($(picArray[picIndex]).find(".img-hldr"),.95,{opacity:1});
+            // gsap.to($(picArray[picIndex]).find(".img-hldr"),3,{ backgroundSize:"100% 100%" });
+            gsap.to($(picArray[picIndex]).find(".img-hldr"),.95,{backgroundSize:"95% 95%",opacity:0,delay:5,onComplete:function(){
+                currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
+                fadeIn(currPic);
+            }});
+        }
+
+    }
+
+    /*-------------------SUPPORT FUNCTIONS-----------------*/
+        function showBlurb(blurbIndex){
+
+            gsap.set($(picArray[blurbIndex]).find(".image-text"),{scale:1.2});
+            gsap.to($(picArray[blurbIndex]).find(".image-text"),2,{scale:1,delay:2});
+            gsap.to($(picArray[blurbIndex]).find(".image-text"),6.5,{opacity:1,ease:Back.easeOut,delay:2});
+            gsap.to(picArray[blurbIndex],.95,{opacity:0,delay:12.5,onComplete:function(){
+                gsap.set($(picArray[blurbIndex]).find(".image-text"),{opacity:0});
+                currPic = blurbIndex === 0 ? picArray.length-1 : blurbIndex - 1 ;
+                fadeIn(currPic);
+            }});
+        }
+        function highlightThumb(){
+            picArray.forEach((thumb,i)=>{
+                if( i === picArray.length-1-currPic ){
+                    $("#thumb_"+i).addClass("actv");
+                }else{
+                    $("#thumb_"+i).removeClass("actv");
+                }
+            });
+        }
+
+    /*--------------------END SUPPORT FUNCTIONS-----------------*/
+
+    /*````````````````````````````````````/
+  /     call main function fadeIn()     /
+/....................................*/
+    fadeIn(currPic);
+
+    $(".catshield").fadeOut(5000);
+
+    /*---------START TEXTFLOW--------*/
+    gsap.to($(".bigBackText"),2100,{x:-10000});
+    gsap.to($(".middleBackText"),2100,{x:-20000});
+    gsap.to($(".smallBackText"),2100,{x:-40000});
+});
