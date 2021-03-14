fc6dbd4589025127773e6220ba72f80577744574
Nucleardelta <ssignmeup@live.com>
2021-03-13 23:19:41 -0500

    Added writings page, sped up timing.

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,25 @@ $(document).ready(function(){
             gsap.set($(picArray[picIndex]).find(".image-text"),{scale:1.2});
             gsap.set($(picArray[picIndex]),{opacity:1});
 
-            gsap.to($(picArray[picIndex]).find(".image-text"),4,{scale:1});
+            gsap.to($(picArray[picIndex]).find(".image-text"),2,{scale:1});
             gsap.to($(picArray[picIndex]).find(".image-text"),2.5,{opacity:1});
 
             // here
             let nextIndex = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
             gsap.set($(picArray[nextIndex]),{opacity:0,backgroundSize:"120% 120%",backgroundPosition:"center center"});
 
-            gsap.to($(picArray[nextIndex]),3,{opacity:1,ease:Power4.easeIn,delay:1});
-            gsap.to($(picArray[nextIndex]),3.5,{ backgroundSize:"100% 100%",delay:2.5 });
+            gsap.to($(picArray[nextIndex]),2,{opacity:1,ease:Power4.easeIn,delay:0});
+            gsap.to($(picArray[nextIndex]),3.5,{ backgroundSize:"100% 100%",delay:0.5 });
             //end here
 
-            gsap.to($(picArray[picIndex]).find(".image-text"),1,{opacity:0,scale:1,delay:3.5,onComplete:function(){
+            gsap.to($(picArray[picIndex]).find(".image-text"),1,{opacity:0,scale:1,delay:1.5,onComplete:function(){
                 currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
                 fadeIn(currPic);
             }});
         }
     //if it's an image div
         else{
-            gsap.to($(picArray[picIndex]),.95,{backgroundSize:"95% 95%",opacity:0,delay:0.5,onComplete:function(){
+            gsap.to($(picArray[picIndex]),.95,{opacity:0,backgroundSize:"95% 95%",delay:0,onComplete:function(){
                 currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
                 fadeIn(currPic);
             }});
@@ -47,6 +47,10 @@ $(document).ready(function(){
     $(document).keyup(function(e){
        // alert("wheel");
    });
+
+   $("#scroll-hldr").scroll(function(){
+     gsap.set($(".left-side"),{y:$("#scroll-hldr").scrollTop()});
+   });
     /*--------------------END SUPPORT FUNCTIONS-----------------*/
 
     /*````````````````````````````````````/
@@ -55,13 +59,12 @@ $(document).ready(function(){
     let url = $(location).attr('href');
     if( url.indexOf("index.html") > -1 ) fadeIn(currPic);
 
-    if( url.indexOf("whatClientsSay.html") > -1 ){
-        gsap.set( $(".image-text"), {opacity:0,x:40} );
-        gsap.to( gsap.utils.toArray(".image-text"), 2,{stagger:.2,opacity:1,x:0,ease:Power4.easeOut} );
-    }
-
-    if( url.indexOf("about") > -1 ||  url.indexOf("contact") > -1 ){
-        gsap.set( $("p"), {opacity:0,x:40} );
-        gsap.to( gsap.utils.toArray("p"), 2,{stagger:.2,opacity:1,x:0,ease:Power4.easeOut} );
+    if( url.indexOf("writings.html") > -1 ){
+        let snippetArray = gsap.utils.toArray(".snippet");
+        snippetArray.forEach((snippet)=>{
+            if($(snippet).html().length > 400){
+                $(snippet).html( $(snippet).html().substring(0, 400) + "... <a href='" + $(snippet).data("id") + ".html' style='color:orange;font-size:10pt'><b>READ THE REST OF THIS ENTRY.</b></a>");
+            }
+        })
     }
 });
