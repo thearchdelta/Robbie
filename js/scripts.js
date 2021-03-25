a046b96b601b60a01182cec52e37726e99e20e2d
Nucleardelta <ssignmeup@live.com>
2021-03-24 23:34:38 -0400

    altered structure

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     let picArray = gsap.utils.toArray(".main-frame");
     let currPic = picArray.length-1;
 
-    function fadeIn(picIndex){
+    function fadeInOld(picIndex){
     //if it's a text div
-        if($(picArray[picIndex]).find(".abc").length !== 0){
-            gsap.set($(picArray[picIndex]).find(".abc"),{scale:1.2});
+        if($(picArray[picIndex]).find(".image-text").length !== 0){
+            gsap.set($(picArray[picIndex]).find(".image-text"),{scale:1.2});
             gsap.set($(picArray[picIndex]),{opacity:1});
 
-            gsap.to($(picArray[picIndex]).find(".abc"),2,{scale:1});
-            gsap.to($(picArray[picIndex]).find(".abc"),2.5,{opacity:1});
+            gsap.to($(picArray[picIndex]).find(".image-text"),2,{scale:1});
+            gsap.to($(picArray[picIndex]).find(".image-text"),2.5,{opacity:1});
 
             // here
             let nextIndex = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
@@ -20,7 +20,7 @@ $(document).ready(function(){
             gsap.to($(picArray[nextIndex]),3.5,{ backgroundSize:"100% 100%",delay:0.5 });
             //end here
 
-            gsap.to($(picArray[picIndex]).find(".abc"),1,{opacity:0,scale:1,delay:1.5,onComplete:function(){
+            gsap.to($(picArray[picIndex]).find(".image-text"),1,{opacity:0,scale:1,delay:1.5,onComplete:function(){
                 currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
                 fadeIn(currPic);
             }});
@@ -28,6 +28,38 @@ $(document).ready(function(){
     //if it's an image div
         else{
             gsap.to($(picArray[picIndex]),.95,{opacity:0,backgroundSize:"95% 95%",delay:0,onComplete:function(){
+                currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
+                fadeIn(currPic);
+            }});
+        }
+
+    }
+
+    function fadeIn(picIndex){
+    //if it's a text div
+        if($(picArray[picIndex]).find(".image-text").length !== 0){
+            gsap.set($(picArray[picIndex]).find(".image-text"),{scale:1.2});
+            gsap.set($(picArray[picIndex]),{opacity:1});
+
+            gsap.to($(picArray[picIndex]).find(".image-text"),2,{scale:1});
+            gsap.to($(picArray[picIndex]).find(".image-text"),2.5,{opacity:1});
+
+            // here
+            let nextIndex = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
+            gsap.set($(picArray[nextIndex]),{opacity:0,backgroundSize:"auto 120%",backgroundPosition:"center center"});
+
+            gsap.to($(picArray[nextIndex]),2,{opacity:1,ease:Power4.easeIn,delay:0});
+            gsap.to($(picArray[nextIndex]),3.5,{ backgroundSize:"auto 100%",delay:0.5 });
+            //end here
+
+            gsap.to($(picArray[picIndex]).find(".image-text"),1,{opacity:0,scale:1,delay:1.5,onComplete:function(){
+                currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
+                fadeIn(currPic);
+            }});
+        }
+    //if it's an image div
+        else{
+            gsap.to($(picArray[picIndex]),.95,{opacity:0,backgroundSize:"auto 95%",delay:0,onComplete:function(){
                 currPic = picIndex === 0 ? picArray.length-1 : picIndex - 1 ;
                 fadeIn(currPic);
             }});
@@ -56,7 +88,7 @@ $(document).ready(function(){
   /     call main function fadeIn()     /
 /....................................*/
     let url = $(location).attr('href');
-    // if( url.indexOf("index.html") > -1 ) fadeIn(currPic);
+    if( url.indexOf("index.html") > -1 ) fadeIn(currPic);
 
     if( url.indexOf("writings.html") > -1 ){
         let snippetArray = gsap.utils.toArray(".snippet");
